66fdf12967b301a1fcf99ba34c674e8f9f217ba6
Sander de Groot <sander@degroot.dev>
2026-04-16 11:24:10 +0200

    fix: add /api prefix to EpistolaCatalogApi controller

--- modules/rest-api/src/main/kotlin/app/epistola/suite/api/v1/EpistolaCatalogApi.kt
+++ modules/rest-api/src/main/kotlin/app/epistola/suite/api/v1/EpistolaCatalogApi.kt
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
+@org.springframework.web.bind.annotation.RequestMapping("/api")
 class EpistolaCatalogApi : CatalogsApi {
 
     override fun importCatalog(
