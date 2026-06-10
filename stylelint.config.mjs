16b0bf6c39417618de5c17a11d4d4f8715f5aa05
whit3st <alican.akyildiz@ieu.edu.tr>
2026-06-10 09:01:14 +0300

    refactor(css): enforce design system token usage

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -9,4 +9,79 @@ export default {
       },
     ],
   },
+  overrides: [
+    {
+      files: ['modules/design-system/base.css', 'modules/design-system/components.css'],
+      rules: {
+        'color-no-hex': [
+          true,
+          {
+            severity: 'error',
+            message:
+              'Use an --ep-* color token instead of %s (see modules/design-system/tokens.css)',
+          },
+        ],
+        'function-disallowed-list': [
+          [
+            'rgb',
+            'rgba',
+            'hsl',
+            'hsla',
+            'hwb',
+            'lab',
+            'lch',
+            'oklab',
+            'oklch',
+            'color',
+            'color-mix',
+          ],
+          {
+            severity: 'error',
+            message:
+              'Use an --ep-* color token instead of %s() (see modules/design-system/tokens.css)',
+          },
+        ],
+        'unit-disallowed-list': [
+          [
+            'px',
+            'em',
+            'rem',
+            'pt',
+            'cm',
+            'mm',
+            'q',
+            'in',
+            'pc',
+            'ch',
+            'ex',
+            'lh',
+            'rlh',
+            'vw',
+            'vh',
+            'vmin',
+            'vmax',
+            'svw',
+            'svh',
+            'lvw',
+            'lvh',
+            'dvw',
+            'dvh',
+            'vb',
+            'vi',
+            'cqw',
+            'cqh',
+            'cqi',
+            'cqb',
+            'cqmin',
+            'cqmax',
+          ],
+          {
+            severity: 'error',
+            message:
+              'Use an --ep-* token instead of "%s" units (see modules/design-system/tokens.css)',
+          },
+        ],
+      },
+    },
+  ],
 };
